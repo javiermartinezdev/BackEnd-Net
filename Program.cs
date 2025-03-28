66d6534567e4e68cec0f7e5adf560dbbdefb4aaf
octaviioViego <octavio.arroyo@estudiante.uacm.edu.mx>
2025-03-28 11:36:36 -0600

    api completa copiada

---
+++ Program.cs
@@ -0,0 +1,94 @@
+using apitienda.Data;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+using productos.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+/// <summary>
+/// Carga las variables de entorno.
+/// </summary>
+Env.Load();
+
+/// <summary>
+/// Obtiene la cadena de conexión a la base de datos desde las variables de entorno o configuración.
+/// </summary>
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); este es la linea original si sale mal la consulta poner esta y quitar la siguiente linea que lee el archivo .env
+var connectionString = builder.Configuration.GetConnectionString("CONNECTIONSTRINGS__DEFAULTCONNECTION");
+
+/// <summary>
+/// Configura el contexto de datos con la base de datos PostgreSQL.
+/// </summary>
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseNpgsql(connectionString)
+);
+
+/// <summary>
+/// Configura el contexto de datos con la base de datos PostgreSQL.
+/// </summary>
+builder.Services.AddDbContext<DataContextProduct>(
+    options => options.UseNpgsql(connectionString)
+);
+
+
+/// <summary>
+/// Registra los servicios de acceso a datos, mapeo y lógica de negocio en la inyección de dependencias.
+/// </summary>
+builder.Services.AddScoped<UsuarioDAO>();
+builder.Services.AddSingleton<UsuarioMapper>();
+builder.Services.AddScoped<UsuarioService>(); // Aquí agregamos UsuarioService
+builder.Services.AddScoped<EmailService>();
+
+builder.Services.AddScoped<IProductDAO, ProductDAO>(); 
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddSingleton<ProductMapper>();
+builder.Services.AddScoped<CreateProductMapper>();
+
+
+/// <summary>
+/// Agrega los servicios necesarios para habilitar los controladores y el modelo MVC.
+/// </summary>
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); // Habilita la exploración de los puntos finales para que Swagger pueda generar documentación
+builder.Services.AddSwaggerGen(); // Agrega el servicio de Swagger para generar documentación interactiva de la API
+
+/// <summary>
+/// Configura la autenticación y autorización.
+/// </summary>
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+
+var app = builder.Build();
+
+/// <summary>
+/// Verifica si el entorno es de desarrollo.
+/// </summary>
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Habilita Swagger en el entorno de desarrollo para generar la documentación interactiva
+    app.UseSwaggerUI(); // Habilita la interfaz de usuario de Swagger para explorar la API
+}
+
+/// <summary>
+/// Habilita la redirección HTTPS.
+/// </summary>
+app.UseHttpsRedirection();
+
+/// <summary>
+/// Habilita la autenticación y autorización.
+/// </summary>
+app.UseAuthentication();
+app.UseAuthorization();
+
+/// <summary>
+/// Mapea los controladores.
+/// </summary>
+app.MapControllers();
+
+/// <summary>
+/// Define una ruta GET para la raíz de la API.
+/// </summary>
+app.MapGet("/", () => "Hola mundo! Nuestra primera API usando C#");
+app.Run();
+
